823ff121a2435be187914c0713d863fcfb3a4428
Jordan Madden <jordanmadden285@gmail.com>
2024-05-24 18:39:09 -0400

    add optimizer class

--- madnet/nn/optim.py
+++ madnet/nn/optim.py
@@ -2,18 +2,25 @@ from madnet.tensor import Tensor
 from .layers import Module
 import numpy as np
 
-class SGD(Module):
-    def __init__(self, parameters, lr=0.001):
-        super().__init__()
-        self.params = parameters
+class Optimizer:
+    def __init__(self, parameters, lr):
+        self.parameters = parameters
         self.lr = lr
+
+    def step(self):
+        raise NotImplementedError
+    
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = np.zeros_like(p.data)
+
+class SGD(Optimizer):
+    def __init__(self, parameters, lr=0.001):
+        super().__init__(parameters, lr)
 
     def __call__(self):
         self.step()
-
-    def parameters(self):
-        return self.params
     
     def step(self):
-        for param in self.parameters():
+        for param in self.parameters:
             param.data = param.data - self.lr * param.grad
