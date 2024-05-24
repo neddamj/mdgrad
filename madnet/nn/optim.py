0d724156e5a4b75335caa9a4688a87690a8d31cf
Jordan Madden <jordanmadden285@gmail.com>
2024-05-24 15:55:14 -0400

    implement SGD class and use it in demo

--- madnet/nn/optim.py
+++ madnet/nn/optim.py
@@ -5,7 +5,18 @@ import numpy as np
 class SGD(Module):
     def __init__(self, parameters, lr=0.001):
         super().__init__()
-        self.parameters = parameters
+        self.params = parameters
+        self.lr = lr
+
+    def __call__(self):
+        self.step()
+
+    def parameters(self):
+        return self.params
+        
+    def zero_grad(self):
+        return super().zero_grad()
     
-    def forward(self):
-        raise NotImplementedError
+    def step(self):
+        for param in self.parameters():
+            param.data = param.data - self.lr * param.grad
