c2bce6142060d32893f14a491f114ab93b5e375c
Jordan Madden <jordanmadden285@gmail.com>
2024-05-29 17:07:01 -0400

    add adam optimizer

--- mdgrad/optim.py
+++ mdgrad/optim.py
@@ -20,5 +20,29 @@ class SGD(Optimizer):
         self.step()
     
     def step(self):
-        for param in self.parameters:
-            param.data = param.data - self.lr * param.grad
+        for p in self.parameters:
+            p.data -= self.lr * p.grad
+
+class Adam(Optimizer):
+    def __init__(self, parameters, lr=0.001, betas=(0.9, 0.999), eps=1e-8):
+        super().__init__(parameters, lr)
+        self.beta1 = betas[0]
+        self.beta2 = betas[1]
+        self.t = 0
+        self.eps = eps
+        self.m = [np.zeros_like(p.data) for p in self.parameters]
+        self.v = [np.zeros_like(p.data) for p in self.parameters]
+
+    def step(self):
+        self.t += 1
+        for i, p in enumerate(self.parameters):
+            # Update biased first moment estimate
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * p.grad
+            # Update biased second raw moment estimate
+            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (p.grad ** 2)
+            # Compute bias-corrected first moment estimate
+            m_hat = self.m[i] / (1 - (self.beta1 ** self.t))
+            # Compute bias corrected second raw moment estimate
+            v_hat = self.v[i] / (1 - (self.beta2 ** self.t))
+            # Update parameters
+            p.data -= self.lr * m_hat / (np.sqrt(v_hat) + self.eps)
