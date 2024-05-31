151b3fb7da03342d0d6a6563b7ee8aa5ba23ec01
Jordan Madden <jordanmadden285@gmail.com>
2024-05-31 16:25:40 -0400

    conv2d forward pass

--- mdgrad/nn/layers.py
+++ mdgrad/nn/layers.py
@@ -64,3 +64,40 @@ class Sequential(Module):
         for layer in self.layers:
             params.extend(layer.parameters())
         return params
+
+class Conv2D(Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.w = Tensor.randn(self.out_channels, self.in_channels, self.kernel_size, self.kernel_size) / np.sqrt(self.kernel_size ** 2)
+        self.b = Tensor.zeros(self.out_channels)
+
+    def forward(self, x):
+        x = x if isinstance(x, Tensor) else Tensor(x)
+        m, n_C_prev, n_H_prev, n_W_prev = x.shape
+        assert self.in_channels == n_C_prev, f'input has {n_C_prev} channels. layer expects {self.in_channels} channels'
+
+        # Create a zero array in the shape of the output of the layer
+        C = self.out_channels
+        H = int((n_H_prev + 2 * self.padding - self.kernel_size) / self.stride + 1)
+        W = int((n_W_prev + 2 * self.padding - self.kernel_size) / self.stride + 1)
+        out = Tensor.zeros((m, C, H, W))
+        # Do convolution
+        for i in range(m):
+            for c in range(C):
+                for h in range(H):
+                    # Slide the filter vertically
+                    h_start = h * self.stride
+                    h_end = h_start + self.kernel_size
+                    for w in range(W):
+                        # Slide the filter horizontally
+                        w_start = w * self.stride
+                        w_end = w_start + self.kernel_size
+                        out[i, c, h, w] = (x[i, :, h_start:h_end, w_start:w_end] * self.w[c, ...]).sum().data + self.b[c]
+
+        return out
